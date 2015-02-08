2be8f0e2ce7bc17ca43148bc7be5be6d77f747f2
Leo Linbeck III <leo3@linbeck.com>
2015-02-08 10:20:19 -0600

    Refactoring for WAK communication

--- BrevitestV6/main.ino
+++ BrevitestV6/main.ino
@@ -6,10 +6,14 @@
 // general
 String serial_number = "OBAD-BWUG-NCPA-WIAF";
 unsigned long start_time;
+char buf[623];
 
 // status
 #define STATUS_LENGTH 1000
 char status[STATUS_LENGTH];
+bool device_ready = false;
+bool init_device = false;
+bool run_assay = false;
 
 // pin definitions
 int pinSolenoid = A1;
@@ -24,21 +28,16 @@ int pinControlSDA = D5;
 int pinControlSCL = D6;
 
 // sensor objects
-#define SENSOR_SAMPLES 10 // number of sensor readings per run
+#define SENSOR_SAMPLES 10
 
 TCS34725 tcsAssay = TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X, pinAssaySDA, pinAssaySCL);
 TCS34725 tcsControl = TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X, pinControlSDA, pinControlSCL);
 String assay_result[2*SENSOR_SAMPLES];
 
 // messaging
-#define MESSAGE_TYPES 8
-String message_types[MESSAGE_TYPES] = { "ID","PING","INIT_DEVICE","INIT_ASSAY","RUN_ASSAY","RESET","DEVICE_STATUS","LAST_READING" };
-String incoming_queue[10];
-int incoming_queue_length = 0;
-String outgoing_queue[10];
-int outgoing_queue_length = 0;
+String spark_register = "";
+String uuid = "";
 int percent_complete = 0;
-bool device_ready = false;
 
 // stepper
 int stepDelay =  1800;  // in microseconds
@@ -62,6 +61,18 @@ int solenoidSurgePeriod = 200; // milliseconds
     int serverPort = 8081;
     bool useDNSserver = false;
 #endif
+
+//
+//
+//  STATUS
+//
+//
+
+void status_update(int count) {
+    if (count >= 0) {
+        Serial.println(status);
+    }
+}
 
 //
 //
@@ -137,7 +148,7 @@ bool limitSwitchOn() {
 }
 
 void reset_x_stage() {
-    status_update(0, snprintf(status, 1000,  "Resetting X stage"));
+    status_update(snprintf(status, STATUS_LENGTH,  "Resetting X stage"));
     move_steps(-30000);
 }
 
@@ -166,10 +177,10 @@ void raster_well(int number_of_rasters) {
 }
 
 void move_to_next_well_and_raster(int path_length, int well_size, const char *well_name) {
-    status_update(0, snprintf(status, 1000, "Moving to %s well", well_name));
+    status_update(snprintf(status, STATUS_LENGTH, "Moving to %s well", well_name));
     move_steps(path_length);
 
-    status_update(0, snprintf(status, 1000, "Rastering %s well", well_name));
+    status_update(snprintf(status, STATUS_LENGTH, "Rastering %s well", well_name));
     raster_well(well_size);
 }
 
@@ -185,11 +196,11 @@ void read_sensor(TCS34725 *sensor, char *sensor_name, int reading_number) {
     t = millis() - start_time;
     sensor->getRawData(&red, &green, &blue, &clear);
 
-    status_update(0, snprintf(status, 1000, "Sensor:\t%s\tt:\t%u\tC:\t%u\tR:\t%u\tG:\t%u\tB:\t%u", sensor_name, t, clear, red, green, blue));
-    assay_result[reading_number] = String(sensor_name[0]) + " \tt:" + String(t) + "\tC:" + String(clear) + "\tR:" + String(red) + "\tG:"  + String(green) + "\tB:" + String(blue);
+    status_update(snprintf(status, STATUS_LENGTH, "Sensor:\t%s\tt:\t%u\tC:\t%u\tR:\t%u\tG:\t%u\tB:\t%u", sensor_name, t, clear, red, green, blue));
+    assay_result[reading_number] = String(sensor_name[0]) + " \tN:" + String(reading_number) + " \tt:" + String(t) + "\tC:" + String(clear) + "\tR:" + String(red) + "\tG:"  + String(green) + "\tB:" + String(blue);
 }
 
-void get_sensor_readings() {
+void collect_sensor_readings() {
     char assay[6] = "Assay";
     char control[8] = "Control";
 
@@ -205,154 +216,107 @@ void get_sensor_readings() {
     analogWrite(pinLED, 0);
 }
 
+//
+//
+//  DATA REQUESTS
+//
+//
+
+void get_sensor_data(String request) {
+    int index = request.toInt();
+    spark_register = assay_result[index];
+}
+
+void get_serial_number() {
+    spark_register = "";
+    for (int i = 0; i < 19; i += 1) {
+        spark_register.setCharAt(i, (char) EEPROM.read(i));
+    }
+}
 
 //
 //
-//  MESSAGE QUEUEING
+//  EXPOSED FUNCTIONS
 //
 //
 
-int queue_message(String msg) {
-    char buf[100];
-
-    msg.toCharArray(buf, 100);
-    status_update(0, snprintf(status, 1000, buf));
-    incoming_queue[incoming_queue_length++] = msg;
+int ping(String msg) {
     return 1;
 }
 
-//
-//
-//  MESSAGE HANDLING
-//
-//
-
-void getCommand(String msg, String *cmd) {
-    *cmd = "";
-    *cmd += msg.substring(0, msg.indexOf(":"));
-    cmd->trim();
-    cmd->toUpperCase();
-}
-
-void getParameters(String msg, String *param) {
-    *param = "";
-    *param += msg.substring(msg.indexOf(":") + 1);
-    param->trim();
-    param->toUpperCase();
-}
-
-void process_message() {
-    String cmd, msg, param;
-    char buf[50];
-    int i;
-
-    msg = incoming_queue[0];
-    incoming_queue_length -= 1;
-    for (i = 0; i < incoming_queue_length; i += 1) {
-        incoming_queue[i] = incoming_queue[i + 1];
-    }
-
-    getCommand(msg, &cmd);
-    getParameters(msg, &param);
-
-    for (i = 0; i < MESSAGE_TYPES; i += 1) {
-        if (cmd.equals(message_types[i])) {
-            break;
+int write_serial_number(String msg) {
+    if (msg.length() == 19) {
+        status_update(snprintf(status, STATUS_LENGTH, "Writing serial number."));
+        for (int i = 0; i < 19; i += 1) {
+            EEPROM.write(i, (uint8_t) msg.charAt(i));
         }
+        return 1;
     }
-
-    switch (i) {
-        case 0: // ID
-            serial_number.toCharArray(buf, 20);
-            status_update(0, snprintf(status, 1000, "Serial number: %s", buf));
-            break;
-        case 1: // PING
-            cmd.toCharArray(buf, 50);
-            status_update(0, snprintf(status, 1000, "Received: %s", buf));
-            break;
-        case 2: // LOAD_DATA
-            break;
-        case 3: // CONFIRM_DATA
-            break;
-        case 4: // RUN_ASSAY
-            if (device_ready) {
-                device_ready = false;
-                run_brevitest();
-            }
-            else {
-                status_update(0, snprintf(status, 1000, "Device not ready. Please reset the device."));
-            }
-            break;
-        case 5: // RESET
-            status_update(0, snprintf(status, 1000, "Resetting device"));
-            reset_device();
-            device_ready = true;
-            break;
-        case 6: // DEVICE_STATUS
-            status_update(0, snprintf(status, 1000, "Status: %s", status));
-            break;
-        case 7: // LAST_READING
-            get_sensor_readings();
-            break;
-        default:
-            status_update(0, snprintf(status, 1000, "Invalid or unknown message."));
+    else {
+        status_update(snprintf(status, STATUS_LENGTH, "Serial number not 19 characters. Writing serial number failed."));
+        return -1;
     }
 }
 
-//
-//
-//  COMMANDS
-//
-//
+int request_data(String msg) {
+    int request_type;
+    String request;
 
-void run_brevitest() {
-    status_update(0, snprintf(status, 1000, "Running BreviTest..."));
+    msg.toCharArray(buf, 63);
+    status_update(snprintf(status, STATUS_LENGTH, "Data request: %s", buf));
+    if (spark_register.length() == 0) {
+        uuid = "" + msg.substring(0, 31);
+        Serial.println(uuid);
+        request_type = msg.substring(32, 33).toInt();
+        Serial.println(request_type);
+        request = "" + msg.substring(34);
+        Serial.println(request);
 
-    move_to_next_well_and_raster(2000, 10, "sample");
-    move_to_next_well_and_raster(1000, 10, "antibody");
-    move_to_next_well_and_raster(1000, 10, "buffer");
-    move_to_next_well_and_raster(1000, 10, "enzyme");
-    move_to_next_well_and_raster(1000, 10, "buffer");
-    move_to_next_well_and_raster(1000, 14, "indicator");
-
-    status_update(0, snprintf(status, 1000, "Reading sensors"));
-    get_sensor_readings();
-
-    status_update(0, snprintf(status, 1000, "Clean up"));
-    reset_device();
-
-    status_update(0, snprintf(status, 1000, "BreviTest run complete."));
+        switch (request_type) {
+            case 0: // serial_number
+                get_serial_number();
+                break;
+            case 1: // sensor data request
+                get_sensor_data(request);
+                break;
+        }
+        return (spark_register == "" ? -1 : 1);
+    }
+    else {
+        spark_register.toCharArray(buf, 622);
+        status_update(snprintf(status, STATUS_LENGTH, "Register locked. Request denied. Register: %s", buf));
+        return -1;
+    }
 }
 
-void reset_device() {
-    solenoid_out();
-    reset_x_stage();
+int release_data(String msg) {
+    Serial.println(msg);
+    if (uuid.equals(msg)) {
+        spark_register = "";
+        return 1;
+    }
+    else {
+        status_update(snprintf(status, STATUS_LENGTH, "Register uuid mismatch. Release denied."));
+        return -1;
+    }
 }
 
-//
-//
-//  STATUS
-//
-//
+int initialize_device(String msg) {
+    init_device = !run_assay;
+    return 1;
+}
 
-void status_update(int mode, int count) {
-    if (count < 0) {
-        return;
+int run_brevitest(String msg) {
+    if (run_assay) {
+        status_update(snprintf(status, STATUS_LENGTH, "Assay already running."));
+        return -1;
     }
-
-    switch (mode) {
-    case 0: // send to serial port
-        Serial.println(status);
-        break;
-    case 1:
-        break;
-    case 2:
-        break;
-    case 3:
-        break;
-    default:
-        break;
+    if (!device_ready) {
+        status_update(snprintf(status, STATUS_LENGTH, "Device not ready. Please reset the device."));
+        return -1;
     }
+    run_assay = true;
+    return 1;
 }
 
 //
@@ -362,11 +326,14 @@ void status_update(int mode, int count) {
 //
 
 void setup() {
-    Spark.function("queue", queue_message);
-    Spark.variable("serialnumber", &serial_number, STRING);
+    Spark.function("ping", ping);
+    Spark.function("writeserial", write_serial_number);
+    Spark.function("requestdata", request_data);
+    Spark.function("releasedata", release_data);
+    Spark.function("initdevice", initialize_device);
+    Spark.function("runassay", run_brevitest);
+    Spark.variable("register", &spark_register, STRING);
     Spark.variable("status", &status, STRING);
-    Spark.variable("queuelength", &incoming_queue_length, INT);
-    Spark.variable("percentdone", &percent_complete, INT);
 
     pinMode(pinSolenoid, OUTPUT);
     pinMode(pinStepperStep, OUTPUT);
@@ -383,27 +350,23 @@ void setup() {
     digitalWrite(pinStepperSleep, LOW);
 
     Serial.begin(9600);
-    while (!Serial.available()) {
-        Spark.process();
-    }
 
     if (tcsAssay.begin()) {
-        status_update(0, snprintf(status, 1000, "Assay sensor initialized"));
+        status_update(snprintf(status, STATUS_LENGTH, "Assay sensor initialized"));
     }
     else {
-        status_update(0, snprintf(status, 1000, "Assay sensor not found"));
+        status_update(snprintf(status, STATUS_LENGTH, "Assay sensor not found"));
     }
 
     if (tcsControl.begin()) {
-        status_update(0, snprintf(status, 1000, "Control sensor initialized"));
+        status_update(snprintf(status, STATUS_LENGTH, "Control sensor initialized"));
     }
     else {
-        status_update(0, snprintf(status, 1000, "Control sensor not found"));
+        status_update(snprintf(status, STATUS_LENGTH, "Control sensor not found"));
     }
 
     start_time = millis();
-
-    status_update(0, snprintf(status, 1000, "Hostname: %s, IP Address: %d.%d.%d.%d", serverName, serverIP[0], serverIP[1], serverIP[2], serverIP[3]));
+    status_update(snprintf(status, STATUS_LENGTH, "Hostname: %s, IP Address: %d.%d.%d.%d", serverName, serverIP[0], serverIP[1], serverIP[2], serverIP[3]));
 }
 
 //
@@ -413,14 +376,32 @@ void setup() {
 //
 
 void loop(){
-    bool incoming = false;
-    int len = 0;
-    char buf[100];
+    if (init_device) {
+        status_update(snprintf(status, STATUS_LENGTH, "Initializing device"));
+        solenoid_out();
+        reset_x_stage();
+        device_ready = true;
+        init_device = false;
+    }
 
-//    websocket_loop();
+    if (device_ready && run_assay) {
+        device_ready = false;
+        status_update(snprintf(status, STATUS_LENGTH, "Running BreviTest..."));
 
-    if (incoming_queue_length > 0) {
-        process_message();
+        move_to_next_well_and_raster(2000, 10, "sample");
+        move_to_next_well_and_raster(1000, 10, "antibody");
+        move_to_next_well_and_raster(1000, 10, "buffer");
+        move_to_next_well_and_raster(1000, 10, "enzyme");
+        move_to_next_well_and_raster(1000, 10, "buffer");
+        move_to_next_well_and_raster(1000, 14, "indicator");
+
+        status_update(snprintf(status, STATUS_LENGTH, "Reading sensors"));
+        collect_sensor_readings();
+
+        status_update(snprintf(status, STATUS_LENGTH, "Clean up"));
+        reset_x_stage();
+        status_update(snprintf(status, STATUS_LENGTH, "BreviTest run complete."));
+        run_assay = false;
     }
 
     delay(500);
