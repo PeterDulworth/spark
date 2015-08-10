c7fca83fd2d60d2cc99880175f1a4200867da553
Leo Linbeck III <leo3@linbeck.com>
2015-08-09 23:53:24 -0500

    QR Test changes

--- QR Test/main.ino
+++ QR Test/main.ino
@@ -11,23 +11,34 @@ int scan_QR_code(String param) {
     unsigned long timeout;
     int buf, i = 0;
 
-    Serial1.begin(9600);  // QR scanner interface through RX/TX pins
+    /*Serial1.flush();*/
 
-    Serial.println("Pulling pinQRTrigger low");
-    digitalWrite(pinQRTrigger, LOW);
+    pull_QR_high("");
 
     timeout = millis() + QR_READ_TIMEOUT;
 
+    while (!Serial1.available() && millis() < timeout) {
+      Spark.process();
+    }
+
+    pull_QR_low("");
+
+    Serial.print("Number of characters read: ");
+    Serial.println(Serial1.available());
+
     do {
         buf = Serial1.read();
-        if (buf == -1) {
-          Serial.print(".");
-        }
-        else {
-          Serial.println(i);
+        if (buf != -1) {
+          Serial.print(i);
+          Serial.print(": ");
+          Serial.print(buf);
+          Serial.print(", hex: ");
+          Serial.print(buf, HEX);
+          Serial.print(", char: ");
           qr_uuid[i++] = (char) buf;
+          Serial.println((char) buf);
         }
-    } while (i < UUID_LENGTH && millis() < timeout);
+    } while (Serial1.available());
     Serial.println();
     Serial.println("Serial1 data collected");
 
@@ -35,14 +46,20 @@ int scan_QR_code(String param) {
     Serial.print("qr_uuid: ");
     Serial.println(qr_uuid);
 
-    Serial.println("Pulling pinQRTrigger high");
-    digitalWrite(pinQRTrigger, HIGH);
-
-    Serial1.end();  // QR scanner interface through RX/TX pins
-
     return 0;
 }
 
+int pull_QR_high(String param) {
+    Serial.println("Pulling pinQRTrigger high");
+    digitalWrite(pinQRTrigger, HIGH);
+    return 0;
+}
+
+int pull_QR_low(String param) {
+    Serial.println("Pulling pinQRTrigger low");
+    digitalWrite(pinQRTrigger, LOW);
+    return 0;
+}
 
 /////////////////////////////////////////////////////////////
 //                                                         //
@@ -52,12 +69,17 @@ int scan_QR_code(String param) {
 
 void setup() {
   Spark.function("scanqrcode", scan_QR_code);
+  Spark.function("pullqrhigh", pull_QR_high);
+  Spark.function("pullqrlow", pull_QR_low);
 
   pinMode(pinQRTrigger, OUTPUT);
-
-  digitalWrite(pinQRTrigger, HIGH);
+  /*pinMode(RX, INPUT);
+  pinMode(TX, OUTPUT);*/
 
   Serial.begin(9600);     // standard serial port
+  Serial1.begin(115200);  // QR scanner interface through RX/TX pins
+
+  pull_QR_low("");
 }
 
 /////////////////////////////////////////////////////////////
@@ -68,5 +90,11 @@ void setup() {
 
 
 void loop(){
+  while (Serial.available()) {
+    Serial1.write(Serial.read());
+  }
 
+  while (Serial1.available()) {
+    Serial.write(Serial1.read());
+  }
 }
